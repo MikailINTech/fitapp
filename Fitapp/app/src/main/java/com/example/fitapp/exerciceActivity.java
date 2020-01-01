20092110762ee0eed49e624da456e2c418b150b1
Mikail Duzenli <mikail@Mikail>
2020-01-01 16:13:42 +0100

    ajout des layout pour chaque classe

--- Fitapp/app/src/main/java/com/example/fitapp/exerciceActivity.java
+++ Fitapp/app/src/main/java/com/example/fitapp/exerciceActivity.java
@@ -1,4 +1,14 @@
 package com.example.fitapp;
 
-public class exerciceActivity {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class exerciceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercice);
+    }
 }
