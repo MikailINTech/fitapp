7c169f1fd7df40c00eacad9ab877da58bbcb5da2
Mikail Duzenli <mikail@Mikail>
2020-01-08 00:05:56 +0100

    j'ai ENFIN REUSSI

--- Fitapp/app/src/main/java/com/example/fitapp/MainActivity.java
+++ Fitapp/app/src/main/java/com/example/fitapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.fitapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         /*Déclaration des boutons*/
         final Button dataButton = findViewById(R.id.dataButton);
         final Button exerciceButton = findViewById(R.id.exerciceButton);
+        final Button googleButton=findViewById(R.id.googleButton);
 
         /*Appel du layout des données*/
         dataButton.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, exerciceActivity.class);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+
+        /*Appel du layout Google Fit*/
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, googleFitActivity.class);
                 startActivityForResult(intent, 0);
 
             }
